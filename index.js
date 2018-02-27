edcbe3903ad041e153ffd899e8678962fda5e0d6
Emily Mame Ford <emilyford352@gmail.com>
2018-02-27 10:56:32 -0500

    Committing uncommitted changes to beeflexa

--- index.js
+++ index.js
@@ -1,72 +1,96 @@
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
 'use strict';
-var Alexa = require('alexa-sdk');
 
-var APP_ID = undefined; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-var SKILL_NAME = 'Beef';
+const Alexa = require('alexa-sdk');
 
-var unirest = require('unirest');
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 
-var theEvent;
-var theContext;
-
-var funny_sayings = [
-    "I dare you to say beef one more time",
-    "Beef, shmeef",
-    "Beefy Eevee",
-    "Duncan's beef",
-    "Kibble and beef",
-    "What kind of beef?",
-    "BEEEEF",
-    "beefy dudes",
-    "beef stroganoff",
-    "Say beef again please",
-    "Beef beef beef beef beef beef beef beef beef",
-    "Beefy baby",
-    "Christmas Beef",
-    "Beef love",
-    "Two beefs walk into a bar, one says beef the other says beef",
-    "Beef some more",
-    "Beef me baby, one more time",
-    "Beef it up",
-    "Beefy",
-    "I love to beef you",
-    "Let me beef that",
-    "Beef me up Scotty",
-    "Toilet beef",
-    "Pig beef",
-    "Eevee then evolved into beef",
-    "Dunky, Eevee and Beefy",
-    "Eevee likes beef",
-    "Why did one beef cross the beef?"
-];
-
-var jokeIntro = ". And now for the joke of the day. ";
-
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    theEvent = event;
-    theContext = context;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+const languageStrings = {
+    'en': {
+        translation: {
+            BEEF: [
+                'I dare you to say beef one more time',
+                'Beef, shmeef',
+                'Beefy Eevee',
+                'Duncan's beef',
+                'Kibble and beef',
+                'What kind of beef?',
+                'BEEEEF',
+                'beefy dudes',
+                'beef stroganoff',
+                'Say beef again please',
+                'Beef beef beef beef beef beef beef beef beef',
+                'Beefy baby',
+                'Christmas Beef',
+                'Beef love',
+                'Two beefs walk into a bar, one says beef the other says beef',
+                'Beef some more',
+                'Beef me baby, one more time',
+                'Beef it up',
+                'Beefy',
+                'I love to beef you',
+                'Let me beef that',
+                'Beef me up Scotty',
+                'Toilet beef',
+                'Pig beef',
+                'Eevee then evolved into beef',
+                'Dunky, Eevee and Beefy',
+                'Eevee likes beef',
+                'Why did one beef cross the beef?'
+            ],
+            SKILL_NAME: 'Space Facts',
+            GET_FACT_MESSAGE: "Here's your fact: ",
+            HELP_MESSAGE: 'Just say beef....',
+            HELP_REPROMPT: 'What can I beef you with?',
+            STOP_MESSAGE: 'Bye, beefy!',
+        },
+    },
 };
 
-var handlers = {
+const handlers = {
     'LaunchRequest': function () {
-        this.emit(':ask', "Beef", SKILL_NAME)
+        this.emit('Beef');
+    },
+    'GetNewFactIntent': function () {
+        this.emit('Beef');
     },
     'Beef': function () {
-            var joke = funny_sayings[Math.floor(Math.random() * funny_sayings.length)];
-                var reply = joke;
-                this.emit(':tell',reply, SKILL_NAME);
+        // Get a random space fact from the space facts list
+        // Use this.t() to get corresponding language data
+        const factArr = this.t('BEEF');
+        const factIndex = Math.floor(Math.random() * factArr.length);
+        const randomFact = factArr[factIndex];
+
+        // Create speech output
+        this.emit(':tell', randomFact);
     },
-    'HelpIntent': function () {
-        var speechOutput = "I am here to say stuff about beef. I dare you to say beef.";
-        var reprompt = "Beef???";
+    'AMAZON.HelpIntent': function () {
+        const speechOutput = this.t('HELP_MESSAGE');
+        const reprompt = this.t('HELP_MESSAGE');
         this.emit(':ask', speechOutput, reprompt);
     },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+};
 
-    'Unhandled': function () {
-        this.emit(':tell', 'Bye, beef!!');
-    }
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
